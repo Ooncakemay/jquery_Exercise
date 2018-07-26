d7842c9683f8a260c0b7cb70bdf76d40e0e37259
Ooncakemay <qscwaz77499@gmail.com>
2018-07-26 11:15:46 +0800

    test

---
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -0,0 +1,47 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplication1
+{
+    public class BundleConfig
+    {
+        // 如需「搭配」的詳細資訊，請瀏覽 http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                    "~/Scripts/jquery-3.3.1.min.js",
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
+            // 準備好實際執行時，請使用 http://modernizr.com 上的建置工具，只選擇您需要的測試。
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                     
+                      "~/Content/AuthModel.Areas.Admin.Content.reset.css",
+                      "~/Content/AuthModel.Areas.Admin.Content.Header.css",
+                      "~/Content/jquery.ui.core.css",
+                      "~/Content/jquery.ui.resizable.css",
+                      "~/Content/jquery.ui.selectable.css",
+                      "~/Content/jquery.ui.accordion.css",
+                      "~/Content/jquery.ui.autocomplete.css",
+                      "~/Content/jquery.ui.button.css",
+                      "~/Content/jquery.ui.dialog.css",
+                      "~/Content/jquery.ui.slider.css",
+                      "~/Content/jquery.ui.tabs.css",
+                      "~/Content/jquery.ui.datepicker..css",
+                      "~/Content/jquery.ui.progressbar.css",
+                      "~/Content/jquery.ui.theme.css",
+                       "~/Content/member.css",
+                      "~/Content/style.css"));
+        }
+    }
+}
---
+++ WebApplication1/Controllers/MemberSearchController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NLog;
+using System.Reflection;
+
+namespace WebApplication1.Controllers
+{
+    public class MemberSearchController : Controller
+    {
+        // GET: MemberSearch
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MemberSearch(string strName, string strContactID,string strMobile,string strPhone, string strEMail)
+        {
+            try
+            {
+                logger.Info("進入首頁，接到參數strName={0}, strContactID = {1},  strMobile = {2}, strPhone = {3}, strEMail  = {4}",
+                    strName, strContactID, strMobile, strPhone, strEMail);
+            }
+            catch(Exception e)
+            {
+                logger.Error("{0 {1}", MethodInfo.GetCurrentMethod().Name, e.Message);
+                throw e;
+            }
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            return View();
+        }
+    }
+}
